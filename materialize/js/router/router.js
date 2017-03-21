58f033c2b6cf6a6475a28974f07d67790aacd18f
Kellorran <souzaluckell@gmail.com>
2017-03-21 15:32:20 -0300

    Angular semi implementado

--- materialize/js/router/router.js
+++ materialize/js/router/router.js
@@ -1,12 +1,12 @@
 angulaAppModulo.config(function($routeProvider) {
     $routeProvider
-        .when('/index', {
-            templateUrl: 'index.html'
-        })
         .when('/montarProvaAberta', {
             templateUrl: 'montarProvaAberta.html'
         })
         .when('/gerarProvas', {
             templateUrl: 'gerarProvas.html'
+        });
+        .when('/template', {
+            templateUrl: 'template.html'
         });
 });
